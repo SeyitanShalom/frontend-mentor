19f629a81f7f721ddb4c75cae13718c53e6f5b49
Arannilewa Seyitan <seyitanarannilewa@gmail.com>
2025-05-26 21:37:24 +0100

    theme button adjustment

--- frontend-mentor-app/src/app/page.js
+++ frontend-mentor-app/src/app/page.js
@@ -30,7 +30,8 @@ export default function Home() {
       </div>
       {/* <button className="">{darkMode ? "LHT" : "DRK"}</button> */}
       <button
-        className=" fixed right-10 bottom-20 px-6 py-2 bg-pink-600 text-white rounded-full hover:bg-pink-800 dark:hover:bg-pink-400 dark:hover:text-white transition-all duration-300 ease-in-out z-50 shadow-lg cursor-pointer"
+        className=" fixed right-5
+         bottom-7 px-6 py-2 bg-pink-600 text-white rounded-full hover:bg-pink-800 dark:hover:bg-pink-400 dark:hover:text-white transition-all duration-300 ease-in-out z-50 shadow-lg cursor-pointer"
         onClick={toggleDarkMode}
       >
         {darkMode ? (
